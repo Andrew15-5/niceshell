4cff366284a8ae45d72f783c22d52760386befbb
Andrew Voynov <andrewvoynov.b@gmail.com>
2022-01-07 17:49:29 +0300

    Updated link_lib.sh for better readability and reliability

--- link_lib.sh
+++ link_lib.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 lib=shell
-current_python=$(basename $(realpath $(which python3)))
 libs_dir=$PWD
-find $libs_dir -type d \( -name .pytest_cache -o -name __pycache__ \) -exec rm -rf '{}' \; 2>/dev/null
-ln -fs "$libs_dir/$lib" ~/".local/lib/${current_python}/site-packages/"
+current_python=$(basename $(realpath $(which python3)))
+destination_dir=~/".local/lib/${current_python}/site-packages"
+find "$libs_dir" -type d \( -name .pytest_cache -o -name __pycache__ \) -exec rm -rf '{}' \; 2>/dev/null
+ln -sf "$libs_dir/$lib" "$destination_dir"
